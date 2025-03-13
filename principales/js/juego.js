59123cbb48f3e2d21d20143214ece15676b79a81
Florencia Maguicha <florenciamailenmaguicha@gmail.com>
2025-03-13 20:14:12 -0300

    Cambios en codigo, estilos y avances en juego

--- principales/js/juego.js
+++ principales/js/juego.js
@@ -4,20 +4,51 @@ document.addEventListener("DOMContentLoaded", () => {
     const tablero = document.getElementById("juego-memoria");
 
 
-    const canciones = [
+    const nombresCanciones = [
         "Colocao", "Wapo Traketero", "Mamichula", "Años Luz", "Frío", "Ella no es Tuya", "Mala Vida", "Parte de Mi"
     ];
 
-    let cartas = [...canciones, ...canciones];
+    let cartas = [...nombresCanciones, ...nombresCanciones];
+    cartas = cartas.sort(() => Math.random() - 0.5);
 
-    cartas = cartas.sort(() => Math.random () - 0.5);
-
-    cartas.forEach(cancion => {
+    cartas.forEach((nombre) => {
         const carta = document.createElement("div");
-            carta.classList.add("carta-memoria");
-            carta.textContent = "?";
-            carta.dataset.nombre = cancion;
-            tablero.appendChild(carta);
+        carta.classList.add("carta-memoria");
+        carta.dataset.nombre = nombre;
+        carta.textContent = "?";
+        carta.addEventListener("click", voltearCarta);
+        tablero.appendChild(carta);
     });
 
+    let cartaVolteada = null;
+    let bloqueoTablero = false;
+
+    function voltearCarta() {
+        if (bloqueoTablero || this.textContent !== "?") return;
+
+        this.textContent = this.dataset.nombre;
+
+        if (!cartaVolteada) {
+            cartaVolteada = this;
+        } else {
+
+            bloqueoTablero = true;
+            setTimeout(() => {
+                this.textContent = "?";
+                cartaVolteada.textContent = "?";
+                cartaVolteada = null;
+                bloqueoTablero = false;
+            }, 1000);
+
+            intentosRestantes--;
+            intentosTexto.textContent = `Intentos restantes: ${intentosRestantes}`;
+
+            if (intentosRestantes === 0) {
+                setTimeout(() => {
+                    alert("¡Perdiste! Se te acabaron los intentos.");
+                    location.reload();
+                }, 500);
+            }
+        }
+    }
 });
